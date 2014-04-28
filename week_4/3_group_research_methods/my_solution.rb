4b2dbcb4a31285564ee759520ceac2b8da84107b
scoin <greg.piccolo@gmail.com>
2014-04-28 17:28:34 -0400

    updated with group work

--- week_4/3_group_research_methods/my_solution.rb
+++ week_4/3_group_research_methods/my_solution.rb
@@ -4,13 +4,26 @@ i_want_pets = ["I", "want", 3, "pets", "but", "I", "only", "have", 2]
 my_family_pets_ages = {"Evi" => 6, "Hoobie" => 3, "George" => 12, "Bogart" => 4, "Poly" => 4, 
             "Annabelle" => 0, "Ditto" => 3}
 
-# Person 1's solution
 def my_array_finding_method(source, thing_to_find)
-  # Your code here!
+    result = []
+    source.each do |x| 
+      if x.class == Fixnum
+        next
+      elsif x.include?(thing_to_find) 
+      result << x
+      end
+    end
+    result
 end
 
 def my_hash_finding_method(source, thing_to_find)
-  # Your code here!
+  result = []
+  source.collect do |k, v| 
+    if v == thing_to_find  
+      result << k 
+    end
+  end
+  result
 end
 
 # Identify and describe the ruby method you implemented. 
@@ -20,18 +33,19 @@ end
 
 # Person 2
 def my_array_modification_method(source, thing_to_modify)
-  # Your code here!
-end
+  result = []
+  source.each_with_index do |element, i|
+    source[i] += thing_to_modify if element.is_a?(Numeric)
+    end
+    source
+  end
 
 def my_hash_modification_method(source, thing_to_modify)
-  # Your code here!
+  source.each do |k,v|
+   source[k] += thing_to_modify
+   end
+   source
 end
-
-# Identify and describe the ruby method you implemented. 
-# 
-#
-#
-
 
 # Person 3
 def my_array_sorting_method(source)
@@ -73,7 +87,7 @@ p my_hash_finding_method(my_family_pets_ages, 3) == ["Hoobie", "Ditto"]
 p my_array_modification_method(i_want_pets, 1) == ["I", "want", 4, "pets", "but", "I", "only", "have", 3 ]
 p my_hash_modification_method(my_family_pets_ages, 2) == {"Evi" => 8, "Hoobie" => 5, "George" => 14, "Bogart" => 6, "Poly" => 6, "Annabelle" => 2, "Ditto" => 5}
 p my_array_sorting_method(i_want_pets) == ["3", "4", "I", "but", "have", "only", "pets", "want"]
-p my_hash_sorting_method(my_family_pets_ages) == [["Annabelle", 2], ["Ditto", 5], ["Hoobie", 5], ["Bogart", 6], ["Poly", 6], ["Evi", 8], ["George", 14]] 
+p my_hash_sorting_method(my_family_pets_ages) == [["Annabelle", 2], ["Ditto", 5], ["Hoobie", 5], ["Poly", 6], ["Bogart", 6], ["Evi", 8], ["George", 14]] 
 # This may be false depending on how your method deals with ordering the animals with the same ages.
 p my_array_deletion_method(i_want_pets, "a") == ["I", 4, "pets", "but", "I", "only", 3 ]
 p my_hash_deletion_method(my_family_pets_ages, "George") == {"Evi" => 8, "Hoobie" => 5, "Bogart" => 6, "Poly" => 6, "Annabelle" => 2, "Ditto" => 5}
