3a8118fd4f1b4690a9bb6acc62d1ceec1361f52f
scoin <greg.piccolo@gmail.com>
2014-05-02 14:10:00 -0400

    credit card complete

--- week_5/6_validate_credit_card/my_solution.rb
+++ week_5/6_validate_credit_card/my_solution.rb
@@ -5,8 +5,8 @@
 
 # 2. Pseudocode
 
-# Input:
-# Output:
+# Input: a credit card number
+# Output: true or false depending if cc holds to algorithm
 # Steps:
 
 
@@ -16,22 +16,61 @@
 # of exactly 16 digits
 class CreditCard
 
+def initialize(ccnum)
+	raise ArgumentError.new "CC must be 16 Digits" if ccnum.to_s.length != 16
+	@ccnum = ccnum
+end
+
+def check_card
+sum = 0
+
+cc_array = @ccnum.to_s.split('').reverse.map.with_index {|d, i| 
+	if i.odd?
+		d.to_i * 2 
+	elsif i.even? 
+		d.to_i 
+	end
+
+}.reverse! # doesn't need to be here, but I feel like I want it in the right order even though we're just summing it
+
+cc_array.each {|i| 
+	if i < 10 
+		sum += i
+	else
+		sum += i.to_s.split('').inject {|both, n| both.to_i + n.to_i}
+	end
+}
+
+return false if sum % 10 != 0
+true
+
+end
+
 end
 
 
 
 # 4. Refactored Solution
 
-
+#I'm going to take a second look at it in a day, but immediately following writing it I cannot find a place
+#to refactor that makes it simpler or more readable.
 
 
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
 
+c = CreditCard.new(4408041234567893)
 
+p c.check_card == true
 
+p c.method(:check_card).arity == 0
 
+d = CreditCard.new(4408041234567892)
 
+p d.check_card == false
 
 # 5. Reflection 
+# This is a fun program. I wrote this in C about 3 months ago and for that reason found it really easy.
+# Ruby is certainly much easier and more intuitive than C. I would like to add features that check what
+# kind of card it is, and if its valid based on the card types requirements (ie Amex must start with 34, 37, or 39.)
