90a74a36e693ab954189f3a75b88ac20e66b8f02
scoin <greg.piccolo@gmail.com>
2014-05-11 21:52:08 -0400

    final challenges

--- week_6/5_refactoring/my_solution.rb
+++ week_6/5_refactoring/my_solution.rb
@@ -1,26 +1,108 @@
 # U2.W6: Refactoring for Code Readability
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself
 
 
 # Original Solution
 
+class CreditCard
 
+def initialize(ccnum)
+	raise ArgumentError.new "CC must be 16 Digits" if ccnum.to_s.length != 16 #argument error
+	@ccnum = ccnum #makes instance variable of ccnum parameter
+end
+
+def check_card
+sum = 0 #sum for later when adding all digits
+
+cc_array = @ccnum.to_s.split('').reverse.map.with_index {|d, i| #maps ccnum string to new array
+	if i.odd? #if the index is odd
+		d.to_i * 2 #multiple the corresponding digit by 2
+	elsif i.even? #if it is even
+		d.to_i #just place in new mapped array unchanged
+	end
+}
+
+cc_array.each {|i| #iterate through new array
+	if i < 10 #if the number is less than 10
+		sum += i #add to sum
+	else #if the number is greater than 10
+		sum += i.to_s.split('').inject {|both, n| both.to_i + n.to_i} #create a string, split the numbers, sum them, then add to sum
+	end
+}
+
+return false if sum % 10 != 0 #return false if the sum of the card number algorithm is not evenly divisable by 10
+true #return true
+
+end
+
+end
 
 
 # Refactored Solution
 
+class CreditCard
 
+def initialize(ccnum)
+		raise ArgumentError.new "Invalid, must start with 3 - 6" if (!(3..6).include?(ccnum.to_s.split('')[0].to_i)) 
+		raise ArgumentError.new "Invalid, not enough digits" if ((4..6).include?(ccnum.to_s.split('')[0].to_i)) && ccnum.to_s.length != 16
+		raise ArgumentError.new "Invalid, Amex must be 15 Digits" if (ccnum.to_s.split('')[0] == '3') && ccnum.to_s.length != 15
+		@sum = 0
 
+		ccnum.to_s.reverse.split('').each_with_index{|n, i| 
+			@sum += (n.to_i * 2).to_s.split('').map(&:to_i).inject(:+) if i.odd?
+			@sum += n.to_i if i.even?
+		}
+end
+
+def check_card
+return false if @sum % 10 != 0
+true
+
+end
+end
 
 
 
 # DRIVER TESTS GO BELOW THIS LINE
 
+#err = CreditCard.new(7408041234567893)
+#err = CreditCard.new(4408041267893)
+#err = CreditCard.new(343841445393274444)
 
 
+c = CreditCard.new(4408041234567893)
+p c.check_card == true
 
+mc = CreditCard.new(5168793266933107)
+p mc.check_card == true
 
+discover = CreditCard.new(6011027511360817)
+p discover.check_card == true
+
+visa = CreditCard.new(4532311568608749)
+p visa.check_card == true
+
+amex = CreditCard.new(343841445393427)
+p amex.check_card == true
+
+amex2 = CreditCard.new(378138389522402)
+p amex2.check_card == true
+
+fake1 = CreditCard.new(4408041234567892)
+p fake1.check_card == false
+
+fake2 = CreditCard.new(4402041239565892)
+p fake2.check_card == false
+
+fake3 = CreditCard.new(378438389222002)
+p fake3.check_card == false
 
 # Reflection 
+# I was happy to refactor my own code. I took a look at it and decided it could be more concise and more readable.
+#I believe I have accomplished that here. Instead of two iterations and two arrays being created, there is
+#now 0 arrays created and one iteration that performs the entire algorithm. The code is only 7 lines, not 
+#including declarations or argument errors. The argument errors as written will only allow card numbers to
+#start with 3,4,5,6, and must have appropriate amount of numbers, (15 if starting with 3, 16 if 4,5,6)
+#It works for all cards, including Amex.
--- week_6/8_BONUS_CarClass/my_solution.rb
+++ week_6/8_BONUS_CarClass/my_solution.rb
@@ -1,7 +1,7 @@
 # U2.W6: Create a Car Class from User Stories
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself
 
 
 # 2. Pseudocode
@@ -10,8 +10,104 @@
 
 # 3. Initial Solution
 
+class Car
+
+	def initialize(model, color, year = 2014)
+		@my_car = "#{color.capitalize} #{year.capitalize} #{model.capitalize}"
+		@speed = 0
+		@mileage = 0.0
+		@pizza_stack = []
+		@last_action = []
+		@last_action << "Created #{@my_car}"
+		puts @last_action[@last_action.size - 1]
+	end
+
+	def drive(distance, speed_limit = @speed)
+		@mileage += distance
+		@speed = speed_limit
+		unless @speed == 0
+			puts "Driving #{distance} miles at #{@speed} MPH"
+			@last_action << "Drove #{distance} miles at #{@speed} MPH"
+		else
+			puts "You are not going anywhere! Tell me how fast to go!"
+		end
+	end
+
+	def accelerate(mph = 5)
+		@speed += mph.abs
+		@last_action << "Accelerated to #{@speed} MPH"
+		puts @last_action[@last_action.size - 1]
+	end
+
+	def slow_down(mph = 5)
+		@speed -= mph.abs
+		if @speed == 0
+			puts "You're already stopped! Get back to work dude!"
+			return
+		end
+		@last_action << "Slowed down to #{@speed} MPH"
+		puts @last_action[@last_action.size - 1]
+	end
+
+	def stop
+		@speed = 0
+		@last_action << "You stopped"
+		puts @last_action[@last_action.size - 1]
+	end
 
 
+	def check_speed
+		puts "You are going #{@speed} MPH"
+		@speed
+	end
+
+	def turn(direction)
+		if direction.downcase == 'left' || direction.downcase == 'right'
+			@last_action << "Turned #{direction}"
+			puts @last_action[@last_action.size - 1]
+			@speed = 0
+		else
+			puts "Turn 'left' or 'right'. What do you think this is, a spaceship?"
+		end
+	end
+
+	def check_mileage
+		puts "You have traveled #{@mileage} miles"
+		@mileage
+	end
+
+	def replay
+		@last_action.each{|string| puts string }
+	end
+
+	def get_pizza(*pies)
+		pies.each{|pie| @pizza_stack << pie 
+		@last_action << "Got #{pie.type} pizza for delivery"}
+		
+		pies.size.downto(1) do |i| puts @last_action[@last_action.size - i] end
+	end
+
+	def deliver_pizza
+		delivered = @pizza_stack.shift
+		if(@speed == 0)
+		@last_action << "Delivered a #{delivered.type} pizza. Well done!"
+		else
+		@last_action << "Threw #{delivered.type} pizza out the window at #{@speed} MPH."
+		end
+		puts @last_action[@last_action.size - 1]
+	end
+end
+
+
+class Pizza
+	def initialize(type = "Plain")
+		@type = type
+	end
+
+	def type
+		@type
+	end
+end
 
 # 4. Refactored Solution
 
@@ -21,10 +117,28 @@
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
-
-
-
+first = Car.new("Oldsmobile 88", "Burgundy", "1989")
+first.get_pizza(Pizza.new("Anchovy"), Pizza.new("Vegan"), Pizza.new("Hawaiian"))
+first.drive(0.25, 25)
+first.stop
+first.turn("right")
+first.drive(1.5, 35)
+first.check_speed
+first.slow_down(20)
+first.drive(0.25)
+first.stop
+first.turn("left")
+first.drive(1.4, 35)
+first.accelerate
+first.accelerate
+first.accelerate(45)
+first.deliver_pizza
+first.stop
+first.deliver_pizza
+first.deliver_pizza
+first.check_mileage
 
 
 
 # 5. Reflection 
+#Super fun one! No real challenge here, just continuing to cement classes. I'm feeling really strong after all this review!
